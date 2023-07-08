0ac050ccae539cba6638bf346927326667b68379
quamarar <122767728+quamarar@users.noreply.github.com>
2023-07-08 21:25:07 +0530

    Update InferencePipeline.groovy

--- jenkins/seed-jobs/InferencePipeline.groovy
+++ jenkins/seed-jobs/InferencePipeline.groovy
@@ -1,1 +1,35 @@
-
+multibranchPipelineJob('ACE_ModelOps/Quality-bu/Inference') {
+    branchSources {
+      branchSource {
+        source {
+         git {
+          id('sample-app')
+          remote('https://gitlab.com/tarandeepsingh009/infrastructure.git')
+          credentialsId('gitlab-token')
+          }
+        }
+        strategy {
+                    defaultBranchPropertyStrategy {
+                        props {
+                            noTriggerBranchProperty()
+                       }
+                  }
+             }
+         }
+    }
+    configure {
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' {}
+    }
+        factory {
+            workflowBranchProjectFactory {
+                scriptPath('jenkins/pipelines/Inference.Jenkinsfile')')
+            }
+        }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+     }
+   }
+}
