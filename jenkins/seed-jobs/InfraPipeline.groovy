ab5fbadda981c2694957fb755b39f2ccedcc7eb4
quamarar <122767728+quamarar@users.noreply.github.com>
2023-07-08 20:53:22 +0530

    Update InfraPipeline.groovy

--- jenkins/seed-jobs/InfraPipeline.groovy
+++ jenkins/seed-jobs/InfraPipeline.groovy
@@ -1,22 +1,44 @@
-pipelineJob('seed-jobs-MSIL-Infra') {
-
-  parameters {
-    stringParam('AWS_REGION','us-east-1','aws region')
-    
-    choiceParam('action', ['plan', 'apply', 'show', 'preview-destroy', 'destroy'], 'Terraform action to apply')
-    choiceParam('ENVIRONMENT', ['dev', 'uat', 'prod'], 'deployment environment',)
-  }
-
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        github('quamarar/quality-mlops-defect-corelation-and-prediction')
-                    }
-                }
-            }
-            scriptPath('jenkins/pipelines/Infra.Jenkinsfile')
-        }
-    }
+folder('ACE_ModelOps') {
+    displayName('Quality-bu')
+    description('Folder for ACE_ModelOps')
 }
+
+ folder('ACE_ModelOps/Quality-bu/quality-mlops-defect-corelation-and-prediction') {
+    multibranchPipelineJob('ACE_ModelOps/Quality-bu/quality-mlops-defect-corelation-and-prediction/seed-jobs') {
+     branchSources {
+      branchSource {
+        source {
+         git {
+          id('seed-jobs')
+
+ remote('https://github.com/quamarar/defect-correlation.git')
+         }
+        }
+        strategy {
+                    defaultBranchPropertyStrategy {
+                        props {
+                            noTriggerBranchProperty()
+                       }
+                  }
+             }
+         }
+    }
+   configure {
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' {}
+    }
+        factory {
+            workflowBranchProjectFactory {
+                scriptPath('jenkins/pipelines/Infra.Jenkinsfile')
+            }
+        }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+     }
+   }
+}
+
+
+
