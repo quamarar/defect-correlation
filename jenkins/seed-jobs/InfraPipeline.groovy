3446359ef720373db177cfc2c270b0c67520afed
quamarar <122767728+quamarar@users.noreply.github.com>
2023-07-08 21:15:27 +0530

    Update InfraPipeline.groovy

--- jenkins/seed-jobs/InfraPipeline.groovy
+++ jenkins/seed-jobs/InfraPipeline.groovy
@@ -1,9 +1,44 @@
 folder('ACE_ModelOps') {
-    displayName('Quality-bu')
+    displayName('ACE_ModelOps')
     description('Folder for ACE_ModelOps')
 }
 
+ folder('ACE_ModelOps/Quality-bu/quality-mlops-defect-corelation-and-prediction') {
+    multibranchPipelineJob('ACE_ModelOps/Quality-bu/quality-mlops-defect-corelation-and-prediction/seed-jobs') {
+     branchSources {
+      branchSource {
+        source {
+         git {
+          id('seed-jobs')
 
+ remote('https://github.com/quamarar/defect-correlation.git')
+         }
+        }
+        strategy {
+                    defaultBranchPropertyStrategy {
+                        props {
+                            noTriggerBranchProperty()
+                       }
+                  }
+             }
+         }
+    }
+   configure {
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' {}
+    }
+        factory {
+            workflowBranchProjectFactory {
+                scriptPath('jenkins/pipelines/Infra.Jenkinsfile')
+            }
+        }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+     }
+   }
+}
 
 
 
