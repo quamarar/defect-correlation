08a4ba56b9b74fefab6f1e3efd3e42c937838395
quamarar <122767728+quamarar@users.noreply.github.com>
2023-07-11 10:34:35 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,27 +1,3 @@
-terraform {
-  required_version = ">= 0.15, < 2.0.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.61.0"
-    }
-    awscc = {
-      source  = "hashicorp/awscc"
-      version = ">= 0.49.0"
-    }
-  }
-}
-
-
-
-provider “aws” {
-  region = “us-east-2”
-  assume_role {
-    role_arn = “arn:aws:iam::667027238366:role/cross-account-role”
-  }
-}
-
-
 
 resource "aws_instance" "app_server" {
   ami           = "ami-06ca3ca175f37dd66"
