b060381ef13adc373628182126b81cb01e7e3648
quamarar <122767728+quamarar@users.noreply.github.com>
2023-07-11 10:08:25 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -17,7 +17,7 @@ terraform {
 
 resource "aws_instance" "app_server" {
   ami           = "ami-06ca3ca175f37dd66"
-  instance_type = "t2.micro
+  instance_type = "t2.micro"
 
   tags = {
     Name = "ExampleAppServerInstance"
