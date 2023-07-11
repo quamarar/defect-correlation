dead09e5c5414a0ffd6b83f19524147f2b943d8c
quamarar <122767728+quamarar@users.noreply.github.com>
2023-07-11 09:46:39 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -11,9 +11,9 @@ terraform {
 
 
 provider “aws” {
- assume_role {
- role_arn = “arn:aws:iam::667027238366:role/cross-account-role”
- } 
+   assume_role {
+   role_arn = “arn:aws:iam::667027238366:role/cross-account-role”
+  }  
  region = “us-east-2”
 }
 
