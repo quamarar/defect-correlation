4c39a093c58da81f374b67ae3bd7025d4c0770dc
quamarar <122767728+quamarar@users.noreply.github.com>
2023-07-11 10:35:04 +0530

    Create providers.tf

---
+++ infra/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 0.15, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.61.0"
+    }
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = ">= 0.49.0"
+    }
+  }
+}
+
+
+
+provider “aws” {
+  region = “us-east-2”
+  assume_role {
+    role_arn = “arn:aws:iam::667027238366:role/cross-account-role”
+  }
+}
