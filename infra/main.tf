500b3a468fbf4dee70bfb171d68d509f6b4677e3
quamarar <122767728+quamarar@users.noreply.github.com>
2023-07-11 10:09:56 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -13,6 +13,13 @@ terraform {
 }
 
 
+provider “aws” {
+   assume_role {
+   role_arn = “arn:aws:iam::667027238366:role/cross-account-role”
+  }  
+ region = “us-east-2”
+}
+
 
 
 resource "aws_instance" "app_server" {
